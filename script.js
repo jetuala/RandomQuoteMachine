e0cf32cd9f9200df7a6990c740f31023c6dd1161
Jonathan Tuala <jetuala@gmail.com>
2018-02-27 11:02:19 -0800

    Random Quote Machine

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-$(document).ready(function() {
+$(document).ready(function(){
+  var data = {
+  method: "getQuote",
+  lang: "en" ,
+  format:"jsonp",
+  dataType: "jsonp"
+  };
+
+  var callQuote = function() {
+    $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?", data, function(jd) {
+      $("#quote").html(jd.quoteText);
+      $("#quoteAuthor").html(jd.quoteAuthor);
+    });
+  };
+
+  callQuote();
+
   $("#newQuote").on("click", function(){
-    $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&key=15456&format=jsonp&jsonp=showQuote&lang=en"), function(json){
-      console.log(json.quoteText);
-      $("#quote").html(JSON.stringify(json.quoteText));
-    };
+  // Only change code below this line.
+    callQuote();
   });
-})
+});
