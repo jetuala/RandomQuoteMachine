dd3a3c0bfc0b8636de40d1bf4e2807fbf978afe4
Jonathan Tuala <jetuala@gmail.com>
2018-02-28 14:20:54 -0800

    Random Quote 2.28

--- script.js
+++ script.js
@@ -7,16 +7,27 @@ $(document).ready(function(){
   };
 
   var callQuote = function() {
-    $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?", data, function(jd) {
-      $("#quote").html(jd.quoteText);
-      $("#quoteAuthor").html(jd.quoteAuthor);
+    $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?", data, function(x) {
+      $("#quote").html(x.quoteText);
+      $("#quoteAuthor").html(x.quoteAuthor);
     });
   };
+
+  var tweetQuote = function(){
+    alert("Twitter button pressed.");
+    var textToTweet = $("#quote").text() + "  - " + $("#quoteAuthor").text();
+    var tweetLink = 'https://twitter.com/intent/tweet?text=' + encodeURIComponent(textToTweet);
+    window.open(tweetLink,'_blank');
+  }
 
   callQuote();
 
   $("#newQuote").on("click", function(){
-  // Only change code below this line.
     callQuote();
   });
+
+  $("#twitterIcon").on("click", function(){
+    tweetQuote();
+  });
+
 });
